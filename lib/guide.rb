5a6e9518b686d374381404e9d960f327385fe7e0
Jisu Kim <jisu.kim@flatironschool.com>
2014-07-23 13:00:35 -0400

    Add details of program in pseudocode

---
+++ lib/guide.rb
@@ -0,0 +1,17 @@
+class Guide
+
+  def initialize(path=nil)
+    # locate the restaurant text file at path
+    # or create a new file
+    # exit if create fails
+  end
+
+  def launch!
+    # introduction
+    # action loop
+    #   what do you want to do?(list, find, add, quit)
+    #   do that action
+    # repeat until user quits
+    # conclusion
+  end
+end
