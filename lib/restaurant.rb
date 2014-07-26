cb0bc980550bfbade74e56e4cc8299986731e498
Jisu Kim <jisu.kim@flatironschool.com>
2014-07-25 22:52:31 -0400

    Add initialize with args hash

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -31,11 +31,18 @@ class Restaurant
   def self.saved_restaurants
   end
 
+  def initialize(args={})
+    @name    = args[:name]    || ""
+    @cuisine = args[:cuisine] || ""
+    @price   = args[:price]   || ""
+  end
+
   def save
     return false unless Restaurant.file_usable?
     File.open(@@filepath, 'a') do |file|
       file.puts "#{[@name, @cuisine, @price].join("\t")}\n"
     end
+    return true
   end
 
 end
